0d3ee1b45f82e793393ea9e3f5ae4277e8d479f8
Larohar <100222459+Larohar@users.noreply.github.com>
2022-02-23 17:40:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ const itemBaseData = {
     "Dumbbells": {name: "Dumbbells", expense: 50, effect: 1.5, description: "Strength XP"},
     "Personal Squire": {name: "Personal Squire", expense: 200, effect: 2, description: "Class XP"},
     "Steel Longsword": {name: "Steel Longsword", expense: 1000, effect: 2, description: "Military XP"},
-    "Magic Stopwatch": {name: "Magic Stopwatch", expense: 2500, effect: 2, description: "Gamespeed"},
+    "Stopwatch": {name: "Stopwatch", expense: 2500, effect: 2, description: "Gamespeed"},
     "Butler": {name: "Butler", expense: 7500, effect: 1.5, description: "Happiness"},
     "Sapphire Charm": {name: "Sapphire Charm", expense: 50000, effect: 3, description: "Magic XP"},
     "Study Desk": {name: "Study Desk", expense: 1000000, effect: 2, description: "Ability XP"},
@@ -202,7 +202,7 @@ const skillCategories = {
 
 const itemCategories = {
     "Properties"             : ["Homeless", "Tent", "Wooden Hut", "Cottage", "House", "Large House", "Small Palace", "Grand Palace", "Town Ruler", "City Ruler", "Nation Ruler", "Pocket Dimension", "Void Realm", "Void Universe", "Astral Realm", "Galactic Throne"],
-    "Misc"                   : ["Book", "Dumbbells", "Personal Squire", "Steel Longsword", "Magic Stopwatch", "Butler", "Sapphire Charm", "Study Desk", "Library", "Stopwatch Overcharge", "Observatory", "Mind's Eye", "Void Necklace", "Void Armor", "Void Blade", "Void Orb", "Void Dust", "Celestial Robe", "Universe Fragment", "Multiverse Fragment"]
+    "Misc"                   : ["Book", "Dumbbells", "Personal Squire", "Steel Longsword", "Stopwatch", "Butler", "Sapphire Charm", "Study Desk", "Library", "Stopwatch Overcharge", "Observatory", "Mind's Eye", "Void Necklace", "Void Armor", "Void Blade", "Void Orb", "Void Dust", "Celestial Robe", "Universe Fragment", "Multiverse Fragment"]
 }
 
 const headerRowColors = {
@@ -345,12 +345,12 @@ const tooltips = {
     "Dumbbells": "Heavy tools used in strenuous exercise to toughen up and accumulate strength even faster than before. ",
     "Personal Squire": "Assists you in completing day to day activities, giving you more time to be productive at work.",
     "Steel Longsword": "A fine blade used to slay enemies even quicker in combat and therefore gain more experience.",
-    "Magic Stopwatch": "A simple tool that helps you shave off some seconds from even the most basic of tasks. Or does it?!",
+    "Stopwatch": "A magic Stopwatch that helps you shave off some seconds from even the most basic of tasks. Or does it?!",
     "Butler": "Keeps your household clean at all times and also prepares three delicious meals per day, leaving you in a happier, stress-free mood.",
     "Sapphire Charm": "Embedded with a rare sapphire, this charm activates more mana channels within your body, providing a much easier time learning magic.",
     "Study Desk": "A dedicated area which provides many fine stationary and equipment designed for furthering your progress in research.",
     "Library": "Stores a collection of books, each containing vast amounts of information from basic life skills to complex magic spells.",
-	"Stopwatch Overcharge": "The Magic Stopwatch gets even more potent when overcharging it.",
+	"Stopwatch Overcharge": "The magic Stopwatch gets even more potent when overcharging it.",
 	"Observatory": "Used for observing terrestrial, marine and celestial events.",
 	"Mind's Eye": "Lets you see memories, remember images, and even see new pictures and ideas.",
 	"Void Necklace": "Helps you shape and manipulate void matter, even transmute it and rebuild into anything of your choosing.",
@@ -571,7 +571,7 @@ function getGameSpeed() {
     var timeWarping = gameData.taskData["Time Warping"]
 	var temporalDimension = gameData.taskData["Temporal Dimension"]
 	var timeLoop = gameData.taskData["Time Loop"]
-	var stopwatch = gameData.itemData["Magic Stopwatch"]
+	var stopwatch = gameData.itemData["Stopwatch"]
 	var stopwatchOC = gameData.itemData["Stopwatch Overcharge"]
 	var stopwatchEffect = stopwatch.getEffect();
 	stopwatchEffect = stopwatchEffect > 1 ? stopwatchEffect * stopwatchOC.getEffect() : 1
@@ -891,7 +891,7 @@ function updateText() {
 	document.getElementById("essenceDisplay").textContent = gameData.essence.toFixed(1)
 	document.getElementById("essenceGainDisplay").textContent = getEssenceGain().toFixed(1)
 
-	var stopwatch = gameData.itemData["Magic Stopwatch"]
+	var stopwatch = gameData.itemData["Stopwatch"]
 	var stopwatchOC = gameData.itemData["Stopwatch Overcharge"]
 	var stopwatchEffect = stopwatch.getEffect();
 	stopwatchEffect = stopwatchEffect > 1 ? stopwatchEffect * stopwatchOC.getEffect() : 1
@@ -1486,7 +1486,7 @@ gameData.requirements = {
 	"Rebirth note 6": new TaskRequirement([document.getElementById("rebirthNote6")], [{task: "Cosmic Recollection", requirement: 1}]),
     "Evil info": new EvilRequirement([document.getElementById("evilInfo")], [{requirement: 1}]),
 	"Essence info": new EssenceRequirement([document.getElementById("essenceInfo")], [{requirement: 1}]),
-    "Time warping info": new CoinRequirement([document.getElementById("timeWarping")], [{requirement: gameData.itemData["Magic Stopwatch"].getExpense() * 100}]),
+    "Time warping info": new CoinRequirement([document.getElementById("timeWarping")], [{requirement: gameData.itemData["Stopwatch"].getExpense() * 100}]),
 
     "Automation": new AgeRequirement([document.getElementById("automation")], [{requirement: 20}]),
     "Quick task display": new AgeRequirement([document.getElementById("quickTaskDisplay")], [{requirement: 20}]),
@@ -1620,7 +1620,7 @@ gameData.requirements = {
     "Dumbbells": new CoinRequirement([getItemElement("Dumbbells")], [{requirement: gameData.itemData["Dumbbells"].getExpense() * 100}]),
     "Personal Squire": new CoinRequirement([getItemElement("Personal Squire")], [{requirement: gameData.itemData["Personal Squire"].getExpense() * 100}]),
     "Steel Longsword": new CoinRequirement([getItemElement("Steel Longsword")], [{requirement: gameData.itemData["Steel Longsword"].getExpense() * 100}]),
-    "Magic Stopwatch": new CoinRequirement([getItemElement("Magic Stopwatch")], [{requirement: gameData.itemData["Magic Stopwatch"].getExpense() * 100}]), 
+    "Stopwatch": new CoinRequirement([getItemElement("Stopwatch")], [{requirement: gameData.itemData["Stopwatch"].getExpense() * 100}]), 
     "Butler": new CoinRequirement([getItemElement("Butler")], [{requirement: gameData.itemData["Butler"].getExpense() * 100}]),
     "Sapphire Charm": new CoinRequirement([getItemElement("Sapphire Charm")], [{requirement: gameData.itemData["Sapphire Charm"].getExpense() * 100}]),
     "Study Desk": new CoinRequirement([getItemElement("Study Desk")], [{requirement: gameData.itemData["Study Desk"].getExpense() * 100}]),
